e2ed778407613af91dec572a5a5e21ae9dfce7ce
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-16 20:20:17 +0530

    test environment added

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+  production: false,
+  test: true,
+  firebase : {
+    apiKey: "AIzaSyC25cgbnqtHtI_2JfDYfQOfjQZVYbAhnNI",
+    authDomain: "clothxtest.firebaseapp.com",
+    databaseURL: "https://clothxtest.firebaseio.com",
+    projectId: "clothxtest",
+    storageBucket: "clothxtest.appspot.com",
+    messagingSenderId: "275075175462"
+  }
+
+
+};
